7824e4fb89a7a8cd5764b763aa131b8eb504bdc9
Sinh <sinhnguyen98hy@gmail.com>
2021-04-04 23:21:56 +0700

    add class match

--- testing/football/models.py
+++ testing/football/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Player(models.Model):
-    name = models.CharField(maxlength=100)
+    name = models.CharField(max_length=100)
+
+
+class Match(models.Model):
+    clubs = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = "match"
